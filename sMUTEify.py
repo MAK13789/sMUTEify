b54f5f3ab9dd55259703efd8567453d35af0e43f
Muhammad Ahsan Kaleem <muhammad.ahsan.kaleem@gmail.com>
2020-12-23 15:23:07 -0500

    figured out how to get access token automatically!! now gonna make the muting part

--- sMUTEify.py
+++ sMUTEify.py
@@ -1,42 +1,25 @@
 '''
 FIGURE OUT HOW TO GET THE ACCESS TOKEN AUTOMATICALLY
+
+ALSO UPDATE THE ACCESS TOKEN EVERY MINUTE OR SOMETHING BECAUSE IT CHANGES (TO DO THIS, MAKE EVERYTHING INTO FUNCTIONS)
+
+WHEN PUTTING ON GITHUB, DON'T SHOW CLIENT ID, SECRET, AND ACCESS TOKEN PUBLICLY
 '''
 import requests
-CLIENT_ID = 'yourclientid'   #hiding for privacy
-CLIENT_SECRET = 'yourclientsecret'   #hiding for privacy
-
-
-'''
-AUTH_URL = 'https://accounts.spotify.com/api/token'
-auth_response = requests.post(AUTH_URL, {
-    'grant_type': 'client_credentials',
-    'client_id': CLIENT_ID,
-    'client_secret': CLIENT_SECRET,
-})
-auth_response_data = auth_response.json()
-access_token = auth_response_data['access_token']
-'''
-
-#access_token taken manually from the website (https://developer.spotify.com/console/get-users-currently-playing-track/?market=&additional_types=):
-access_token = 'youraccesstoken'   #hiding for privacy
-
-
+CLIENT_ID = 'a455f73c6eac46428cb7f35d75e842f8'
+CLIENT_SECRET = '9bf9b5fce0b147dca5adb1758711e85d'
+import spotipy.util as util
+username = 'Ahsan Kaleem' 
+redirect_uri = 'http://localhost:7777/callback'
+scope = 'user-read-currently-playing'
+access_token = util.prompt_for_user_token(username=username, 
+                                   scope=scope, 
+                                   client_id=CLIENT_ID,   
+                                   client_secret=CLIENT_SECRET,     
+                                   redirect_uri=redirect_uri)
 headers = {
     'Authorization': 'Bearer {token}'.format(token=access_token)
 }
-
-
-
-#getting info for a song (just using this as an example to test this stuff):
-'''
-BASE_URL = 'https://api.spotify.com/v1/'
-track_id = '7lQWRAjyhTpCWFC0jmclT4'
-r = requests.get(BASE_URL + 'audio-features/' + track_id, headers=headers)
-r = r.json()
-print (r)
-'''
-
-
 r = requests.get('https://api.spotify.com/v1/me/player/currently-playing', headers=headers)
 r = r.json()
 print (r)
